c4d65ac02d38428234b2f27079cda5bb6c652dc1
arashimoya <arasimovicadam@gmail.com>
2022-05-17 22:00:17 +0200

    chat styling

--- app/src/main/java/com/example/taxiapp/Message.java
+++ app/src/main/java/com/example/taxiapp/Message.java
@@ -1,20 +1,29 @@
 package com.example.taxiapp;
 
+import com.google.firebase.Timestamp;
+
 public class Message {
     private String name;
     private String message;
     private String date;
     private String time;
+    private String userId;
+    private Timestamp timestamp;
 
     public Message(){}
 
-    public Message(String name, String message, String date, String time) {
+    public Message(String name, String message, String date, String time, Timestamp timestamp, String userId) {
         this.name = name;
         this.message = message;
         this.date = date;
         this.time = time;
+        this.timestamp = timestamp;
+        this.userId = userId;
     }
 
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
 
     public String getDate() {
         return date;
@@ -31,6 +40,11 @@ public class Message {
     public String getTime() {
         return time;
     }
+
+    public String getUserId(){
+        return userId;
+    }
+
 
 
 
--- app/src/main/java/com/example/taxiapp/MessageAdapter.java
+++ app/src/main/java/com/example/taxiapp/MessageAdapter.java
@@ -1,6 +1,8 @@
 package com.example.taxiapp;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,39 +11,46 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
 
     Context context;
-    ArrayList<Message> messagesArrayList;
+    List<Message> messagesArrayList;
+    private FirebaseAuth fAuth;
 
-    public MessageAdapter(Context context, ArrayList<Message> messagesArrayList) {
+    public MessageAdapter(Context context, List<Message> messagesArrayList) {
         this.context = context;
         this.messagesArrayList = messagesArrayList;
+        fAuth = FirebaseAuth.getInstance();
     }
 
     @NonNull
     @Override
     public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(context).inflate(R.layout.message_item,parent,false);
-
-
-
+        View v = LayoutInflater.from(context)
+                .inflate(R.layout.message_item,parent,false);
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MessageAdapter.ViewHolder holder, int position) {
-
         Message message = messagesArrayList.get(position);
-
+        if(message.getName().equals(fAuth.getCurrentUser().getUid()))
+        holder.itemView.setBackgroundColor(Color.parseColor("#FFBB86FC"));
         holder.date.setText(message.getDate());
         holder.message.setText(message.getMessage());
         holder.user.setText(message.getName());
         holder.time.setText(message.getTime());
+        String toPrint = message.getUserId();
+        Log.d("messageAdp", toPrint);
+        Log.d("messageAdp",fAuth.getUid());
+
 
     }
 
@@ -49,6 +58,11 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     public int getItemCount() {
         return messagesArrayList.size();
     }
+
+    public void setMessagesArrayList(ArrayList<Message> messagesArrayList) {
+        this.messagesArrayList = messagesArrayList;
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView date, message, user, time;
